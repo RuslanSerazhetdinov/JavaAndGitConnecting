6b4e04f2b5185956403c7708d063afa0689b508e
Serazhetdinov Ruslan <paketikno@gmail.com>
2023-11-01 18:05:41 +0300

    Continue Learn

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        System.out.println("¬ведите свой вес ( г)");
+        double weight = new Scanner(System.in).nextDouble();
+        System.out.println("¬ведите свой рост (м)");
+        double height = new Scanner(System.in).nextDouble();
+
+        System.out.println("BMI = " + weight / (height * height));
+        */
+
+        /*
+        System.out.println("\t\t***ƒобро пожаловать в калькул€тор умножени€ чисел!***\n\n");
+        System.out.println("¬ведите первое число:");
+        int a = new Scanner(System.in).nextInt();
+        System.out.println("¬ведите второе число:");
+        int b = new Scanner(System.in).nextInt();
+
+        System.out.println(a + " * " + b + " = " + a * b);
+        */
+
+
+    }
+}
